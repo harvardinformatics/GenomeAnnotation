c091b587fa00b2443adf0b07f206598530698d6e
afreedman <adamfreedman@fas.harvard.edu>
2022-02-10 13:31:02 -0500

    updated slurm settings for braker scripts

--- Braker/slurm_scripts/braker_danaus_plexippus_proteinonly_example.sh
+++ Braker/slurm_scripts/braker_danaus_plexippus_proteinonly_example.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 # Customize --time and --partition as appropriate.
 # --exclusive --mem=0 allocates all CPUs and memory on the node.
-#SBATCH --partition=holy-info,holy-smokes,holy-cow,shared
+#SBATCH --partition=shared
 #SBATCH --nodes=1
-#SBATCH --mem=0
 #SBATCH --exclusive
+#SBATCH --mem=0
 #SBATCH -J brakrnaseq
-#SBATCH --time=2-23:00
+#SBATCH --time=3-23:00:00
 
 
 singularity exec --cleanenv /n/singularity_images/informatics/braker/braker_2021-06-14.sif cp -Rs /opt/augustus/config augustus_config
 singularity exec --no-home \
                  --home /opt/gm_key \
                  --cleanenv \
-                 --env AUGUSTUS_CONFIG_PATH=${PWD}/augustus_config\
+                 --env AUGUSTUS_CONFIG_PATH=${PWD}/augustus_config \
                  /n/singularity_images/informatics/braker/braker_2021-06-14.sif braker.pl --cores 48 \
                  --species=danaus_plexippus --genome=dple.fa \
-                 --prot_seq=/n/holyscratch01/informatics/genome_annotation_evaluation/heliconines/hmel_refdata/headcleaned_Hmel2_braker_20170203.proteins.renamed.Hm.faa \
+                 --prot_seq=/n/holyscratch01/external_repos/INFORMATICS/orthodb/orthoDBv100_arthropoda.fasta \
                  --softmasking 
--- Braker/slurm_scripts/braker_danaus_plexippus_rnaseq_example.sh
+++ Braker/slurm_scripts/braker_danaus_plexippus_rnaseq_example.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 # Customize --time and --partition as appropriate.
 # --exclusive --mem=0 allocates all CPUs and memory on the node.
-#SBATCH --partition=holy-info,holy-smokes,holy-cow
+#SBATCH --partition=shared
 #SBATCH --nodes=1
-#SBATCH --mem=0
-#SBATCH --exclusive
+#SBATCH -n 24
+#SBATCH --mem=75000
 #SBATCH -J brakrnaseq
-#SBATCH --time=3-23:00
-
+#SBATCH --time=23:00:00
 
 singularity exec --cleanenv /n/singularity_images/informatics/braker/braker_2021-06-14.sif cp -Rs /opt/augustus/config augustus_config
 singularity exec --no-home \
                  --home /opt/gm_key \
                  --cleanenv \
                  --env AUGUSTUS_CONFIG_PATH=${PWD}/augustus_config\
-                 /n/singularity_images/informatics/braker/braker_2021-06-14.sif braker.pl --cores 48 \
-                 --hints=filtered_namesorted_dple_hisat-star_merged.hints.gff \
+                 /n/singularity_images/informatics/braker/braker_2021-06-14.sif braker.pl --cores 24 \
                  --bam=dple_hisat-star_merged.bam \
                  --species=danaus_plexippus --genome=dple.fa \
-                 --softmasking \
-                 --UTR=on
+                 --softmasking 
