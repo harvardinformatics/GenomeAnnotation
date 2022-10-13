5c7e5730549ba42514b5e88020ee04376b723a21
afreedman <adamfreedman@fas.harvard.edu>
2022-10-13 10:32:43 -0400

    added and tested fastasplitter script

---
+++ Snakemake/RnaReads2CDSAnnotation/workflow/scripts/FastaSplitter.py
@@ -0,0 +1,27 @@
+import argparse
+from Bio import SeqIO
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='split fasta files into mulitple files of max specified number of seqs')
+    parser.add_argument('-f','--fasta-infile',dest='fastain',type=str,help='input fasta file')
+    parser.add_argument('-maxn','--max-number-records',dest='maxn',type=int,help='max number of fasta records per split file')
+    opts = parser.parse_args()
+    
+    file_index = 1
+    seqcount = 0
+    fout = open('%s_%s' % (file_index,opts.fastain),'w')
+    
+    for record in SeqIO.parse(opts.fastain,'fasta'):
+        if seqcount <=opts.maxn-1:
+            SeqIO.write(record,fout,'fasta')
+            seqcount+=1
+        else:
+            file_index+=1
+            fout.close()
+            fout = open('%s_%s' % (file_index,opts.fastain),'w') 
+            SeqIO.write(record,fout,'fasta')
+            seqcount=1 
+             
+
+    fout.close()
