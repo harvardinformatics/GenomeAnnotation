68e18a8b928514c7c57d343ebd8e31b8b0178d9b
afreedman <adamfreedman@fas.harvard.edu>
2022-10-13 11:13:19 -0400

    added subfile counter and write of counter file

--- Snakemake/RnaReads2CDSAnnotation/workflow/scripts/FastaSplitter.py
+++ Snakemake/RnaReads2CDSAnnotation/workflow/scripts/FastaSplitter.py
@@ -6,6 +6,7 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description='split fasta files into mulitple files of max specified number of seqs')
     parser.add_argument('-f','--fasta-infile',dest='fastain',type=str,help='input fasta file')
     parser.add_argument('-maxn','--max-number-records',dest='maxn',type=int,help='max number of fasta records per split file')
+    parser.add_argument('-counter','--number-subfiles-out',dest='numfiles',type=int,help='output file name for count of subfiles')
     opts = parser.parse_args()
     
     file_index = 1
@@ -22,6 +23,8 @@ if __name__=="__main__":
             fout = open('%s_%s' % (file_index,opts.fastain),'w') 
             SeqIO.write(record,fout,'fasta')
             seqcount=1 
-             
-
     fout.close()
+
+    counterout = open('%s' % opts.numfiles,'w')
+    counterout.write('%s\n' % file_index)
+    counterout.close()
