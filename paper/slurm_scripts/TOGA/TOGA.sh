fe8774597df055437914ae7208159e5bb85efcaf
afreedman <adamfreedman@fas.harvard.edu>
2022-12-19 06:18:16 -0500

    another change of source to query for variable names

--- paper/slurm_scripts/TOGA/TOGA.sh
+++ paper/slurm_scripts/TOGA/TOGA.sh
@@ -14,8 +14,8 @@ source activate TOGA
 chainfile=$1
 targetCDSbed=$2
 target2bit=$3
-source2bit=$4
+query2bit=$4
 outname=$5
 isoforms=$6 # tsv file with CDS gene and transcript id as columns
 
-/n/home_rc/afreedman/software/TOGA/toga.py $chainfile $targetCDSbed $target2bit $source2bit --kt --pn $outname -i $isoforms  --nc /n/home_rc/afreedman/software/TOGA/my_nextflow_config_files --cb 10,100 --cjn 750 
+/n/home_rc/afreedman/software/TOGA/toga.py $chainfile $targetCDSbed $target2bit $query2bit --kt --pn $outname -i $isoforms  --nc /n/home_rc/afreedman/software/TOGA/my_nextflow_config_files --cb 10,100 --cjn 750 
