27b9d018360a8d2227f9e01b651c5852d3634f52
afreedman <adamfreedman@fas.harvard.edu>
2023-12-11 09:49:35 -0500

    added GC content calculation to repeat fraction script

--- PerformanceMetrics/utilities/WriteRepeatFractionGCcontentTableFromTranscriptomeFasta.py
+++ PerformanceMetrics/utilities/WriteRepeatFractionGCcontentTableFromTranscriptomeFasta.py
@@ -1,16 +1,17 @@
 import sys
-from Bio import SeqIO
+from Bio import SeqIO,SeqUtils
 from numpy import mean,median
 
 tsfasta = open(sys.argv[1],'r')
-output = open('%s_repeatfractions.tsv' % sys.argv[1],'w')
-output.write('transcriptID\tsoftmaskprop\thardmaskprop\ttotalrepprop\n')
+output = open('%s_repeatfractions_gc.tsv' % sys.argv[1],'w')
+output.write('transcriptID\tsoftmaskprop\thardmaskprop\ttotalrepprop\tGCcontent\n')
 
 sm_fracs = []
 hm_fracs = []
 total_fracs = []
 for record in SeqIO.parse(tsfasta,'fasta'):
     transcript_length=len(record.seq)
+    gc = SeqUtils.GC(record.seq)
     softmask_base_count = 0
     hardmask_base_count = 0
     for base in record.seq:
@@ -18,8 +19,6 @@ for record in SeqIO.parse(tsfasta,'fasta'):
             hardmask_base_count+=1
         elif base.islower() == True and base != 'n':
             softmask_base_count+=1
-        #elif base not in ['A','C','G','T']:
-            #print('%s found in %s, is not an accepted nucleotide' % (base,record.id))
 
     softmaskprop = softmask_base_count/float(transcript_length)
     hardmaskprop = hardmask_base_count/float(transcript_length)
@@ -29,7 +28,7 @@ for record in SeqIO.parse(tsfasta,'fasta'):
     hm_fracs.append(hardmaskprop)
     total_fracs.append(totalmaskprop)    
 
-    output.write('%s\t%s\t%s\t%s\n' % (record.id,softmaskprop,hardmaskprop,totalmaskprop))
+    output.write('%s\t%s\t%s\t%s\t%s\n' % (record.id,softmaskprop,hardmaskprop,totalmaskprop,gc))
 
 output.close()
 stats = open('%s.repeatstats' % sys.argv[1],'w')
@@ -42,6 +41,9 @@ stats.write('median hardmask fraction: %s\n' % median(hm_fracs))
 stats.write('mean soft+hard masked fraction: %s\n' % mean(total_fracs))
 stats.write('median soft+hard masked fraction: %s\n' % median(total_fracs))
 stats.write('max soft+hard mask fraction: %s\n' % max(total_fracs))
+
+stats.write('mean gc content: %s\n' % mean(gc))
+stats.write('median gc content: %s\n' % median(gc))
 stats.close()
 
 
